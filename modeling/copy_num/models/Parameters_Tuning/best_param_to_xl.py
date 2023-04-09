5f7d8d74028d58a9e1441026ca900c3ca1551bba
yamtawa <122857307+yamtawa@users.noreply.github.com>
2023-04-09 19:36:41 +0300

    best params finctions

--- modeling/copy_num/models/Parameters_Tuning/best_param_to_xl.py
+++ modeling/copy_num/models/Parameters_Tuning/best_param_to_xl.py
@@ -1,18 +1,35 @@
+
 import pandas as pd
-import os.path
-from openpyxl import load_workbook
+import openpyxl
+import os
+def write_to_xl(dic,model_name):
+    df1=pd.DataFrame(dic.values())
+    df1['scores']=dic.keys()
+    xl_name=f'{model_name}_best_params.xlsx'
+    target_file=os.path.join(os.getcwd(), xl_name)
+    try:
+        df2 = pd.read_excel(target_file)
+    except:
+        print(f'create xl {xl_name}')
+        workbook = openpyxl.Workbook()
+        workbook.save(target_file)
+        df2=pd.read_excel(target_file)
+    df= pd.concat([df2, df1], ignore_index=True)
+    df.to_excel(target_file,index=False)
 
-def best_params_to_xl(d_params, model_name):
-    df = pd.DataFrame(d_params.values())
-    df['score'] = d_params.keys()
-    path = 'Best_params.xlsx'
-    sheet_exists = os.path.isfile(path) and model_name in load_workbook(path).sheetnames
-    with pd.ExcelWriter(path, mode='a' if sheet_exists else 'w') as writer:
-        if sheet_exists:
-            writer.book = load_workbook(path)
-        df.to_excel(writer, sheet_name=f'{model_name} best params')
 
-best_params_to_xl({1.02:{'f':131,'d':4}}, 'yam')
+def get_best_params_set(model_name):
+    xl_name = f'{model_name}_best_params.xlsx'
+    df=pd.read_excel(xl_name)
+    score=df['scores'].max()
+    params=df.iloc[df['scores'].idxmax(),:].dropna().drop('scores')
+    print(f'Best params for {model_name} model are:\n{params}\nAnd their predicted score is {score}')
+    return(params)
+## example##
+# dic={1200:{'r':4,'t':300}}
+# model_name='xgb'
+# write_to_xl(dic,model_name)
+# a=get_best_params_set(model_name)
 
 
 
