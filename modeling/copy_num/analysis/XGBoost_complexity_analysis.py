5c9bbdd88160c110139668dbe4d1d23d4057c531
Rinat Saban <rinatsaban7@gmail.com>
2023-05-16 13:32:27 +0300

    complexity analysis finished

--- modeling/copy_num/analysis/XGBoost_complexity_analysis.py
+++ modeling/copy_num/analysis/XGBoost_complexity_analysis.py
@@ -5,6 +5,39 @@ import os
 
 FIGURES_PATH = os.path.join("..", "..", "..", "data", "copy_num", "figures")
 
+
+def elbow_graphs(estimator, estimator_range, X, y, data_name):
+    r2_list = []
+    sp_list = []
+    for estimator_value in estimator_range:
+        r2, mse_score, spearman = model(X, y, model_name="xgboost", data_name=data_name,
+                              Best_param={estimator: estimator_value}, save_plots=False)
+        r2_list.append(r2)
+        sp_list.append(spearman)
+
+    # plot
+    fig, ax1 = plt.subplots()
+
+    color = 'tab:red'
+    ax1.set_xlabel('number of trees' if estimator=='n_estimators' else 'maximum depth of a tree')
+    ax1.set_ylabel('r2 score', color=color)
+    ax1.plot(estimator_range, r2_list, color=color)
+    ax1.scatter(estimator_range, r2_list, color=color)
+    ax1.tick_params(axis='y', labelcolor=color)
+
+    ax2 = ax1.twinx()
+    color = 'tab:blue'
+    ax2.set_ylabel('spearman', color=color)  # we already handled the x-label with ax1
+    ax2.plot(estimator_range, sp_list)
+    ax2.scatter(estimator_range, sp_list)
+    ax2.tick_params(axis='y', labelcolor=color)
+
+    plt.title(f"XGBoost {estimator} vs. scores for {data_name}")
+    fig.tight_layout()
+
+    plt.savefig(os.path.join(FIGURES_PATH, f'XGBoost {estimator} vs scores {data_name}.jpg'))
+
+
 if __name__ == '__main__':
     data = get_features_df()
     RNAp_X = data['RNAp_X']
@@ -14,21 +47,11 @@ if __name__ == '__main__':
     X_shared_model = data['X_shared']
     Y_shared_model = data['Y_shared']
 
-    n_estimators_range = range(4, 1000, 20)
-    r2_list = []
-    mse_list = []
-    for n_estimators in n_estimators_range:
-        r2, mse_score = model(RNAp_X, RNAp_y, model_name="xgboost", data_name="pRNA",
-                              Best_param={'n_estimators': n_estimators}, save_plots=False)
-        r2_list.append(r2)
-        mse_list.append(mse_score)
+    elbow_graphs('n_estimators', range(10, 300, 10), RNAp_X, RNAp_y, 'RNAp')
+    elbow_graphs('max_depth', range(2, 20, 1), RNAp_X, RNAp_y, 'RNAp')
 
-    # plot
-    fig = plt.figure()
-    plt.errorbar(n_estimators_range, r2_list)
-    # plt.errorbar(n_estimators_range, mse_list)
+    elbow_graphs('n_estimators', range(10, 300, 10), RNAi_X, RNAi_y, 'RNAi')
+    elbow_graphs('max_depth', range(2, 20, 1), RNAi_X, RNAi_y, 'RNAi')
 
-    plt.title("XGBoost n_estimators vs R2 score")
-    plt.xlabel('n_estimators')
-    plt.ylabel('R2')
-    plt.savefig(os.path.join(FIGURES_PATH, f'XGBoost n_estimators vs R2 score pRNA.jpg'))
+    elbow_graphs('n_estimators', range(10, 300, 10), X_shared_model, Y_shared_model, 'shared model')
+    elbow_graphs('max_depth', range(2, 20, 1), X_shared_model, Y_shared_model, 'shared model')
