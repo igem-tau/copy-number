946a10c80892ab7cdf6b6cbf7f4bf7704dcad2b6
Eden Dotan <edendotan@mail.tau.ac.il>
2023-05-21 18:21:16 +0300

    add an option to cancel oulires removal, and a new function to split the data into 3 parts: train, validation and test

--- modeling/copy_num/models/models_functions.py
+++ modeling/copy_num/models/models_functions.py
@@ -20,8 +20,9 @@ def remove_outlires(X: pd.DataFrame, y: pd.DataFrame):
     y = y[(y>lower_fence) & (y<higher_fence)]
     return X, y
 
-def prepare_model_data(X: pd.DataFrame, y: pd.DataFrame):
-    X, y = remove_outlires(X, y)
+def prepare_model_data(X: pd.DataFrame, y: pd.DataFrame, outlires=True):
+    if outlires:
+        X, y = remove_outlires(X, y)
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.15, random_state=0,stratify=is_high_copy_number(y))
     numeric_features = X_train.select_dtypes(include='float64', exclude='int64')
 
@@ -44,3 +45,13 @@ def model(train_test,X: pd.DataFrame, y: pd.DataFrame, model_name: str, data_nam
         run_xgboost(X_train, X_test, y_train, y_test, data_title=data_name, Best_param=Best_param)
     else:
         raise Exception(f"No such model: {model_name}")
+
+def train_validation_test_split(X, y, random_stat):
+    stratify_col = pd.DataFrame(list(is_high_copy_number(y)), columns=['stratify'])
+    X = pd.concat((X, stratify_col), axis=1)
+    X_train, X_temp, y_train, y_temp = train_test_split(X, y, test_size=0.3, random_state=random_stat, stratify=X['stratify'])
+    X_valid, X_test, y_valid, y_test = train_test_split(X_temp, y_temp, test_size=0.5, random_state=random_stat,
+                                                        stratify=X_temp['stratify'])
+
+    return (X_train.drop('stratify', axis=1), X_valid.drop('stratify', axis=1), X_test.drop('stratify', axis=1),
+            y_train, y_valid, y_test)
