6b70aa862a4df4f2b8f4115778c35bc5e088671b
Eyal_bitdam <eyal.blyachman@bitdam.com>
2023-06-18 15:25:01 +0300

    added stuff I worked before

--- utils/ref_seqs.py
+++ utils/ref_seqs.py
@@ -8,6 +8,7 @@ from Bio.Align.Applications import MuscleCommandline
 from Bio.Align import substitution_matrices
 import os
 import pandas as pd
+from typing import List
 
 
 def download_reference_fasta(reference, output_path):
@@ -101,7 +102,8 @@ def get_promoter_alignment(fasta_file, prom_seq):
     for record in SeqIO.parse(fasta_file, "fasta"):
         sequence = str(record.seq)
 
-        sub_mat = substitution_matrices.load("BLOSUM62")
+        sub_mat = substitution_matrices.load("NUC.4.4")
+        pairwise2.MAX_ALIGNMENTS = 10
         alignments = pairwise2.align.localds(prom_seq, sequence, sub_mat,
                                              gap_open_penalty, gap_extend_penalty)
 
@@ -120,21 +122,61 @@ def get_align_fastas_dict(fastas_dir, prom_seq):
     return align_dict
 
 
-RNA_P = "gcaaacaaaaaaaccaccgctaccagcggtggtttgtttgccggatcaagagctaccaactctttttccgaaggtaactggcttcagcagagcgcagataccaaatactgttcttctagtgtagccgtagttaggccaccacttcaagaactctgtagcaccgcctacatacctcgctctgctaatcctgttaccagtggctgctgccagtggcgataagtcgtgtcttaccgggttggactcaagacgatagttaccggataaggcgcagcggtcgggctgaacggggggttcgtgcacacagcccagcttggagcgaacgacctacaccgaactgagatacctacagcgtgagctatgagaaagcgccacgcttcccgaagggagaaaggcggacaggtatccggtaagcggcagggtcggaacaggagagcgcacgagggagcttccagggggaaacgcctggtatctttatagtcctgtcgggtttcgccacctctgacttgagcgtcgatttttgtgatgctcgtcaggggggcggagcctatggaaa"
+RNA_P = "gcaaacaaaaaaaccaccgctaccagcggtggtttgtttgccggatcaagagctaccaactctttttccgaaggtaactggcttcagcagagcgcagataccaaatactgttcttctagtgtagccgtagttaggccaccacttcaagaactctgtagcaccgcctacatacctcgctctgctaatcctgttaccagtggctgctgccagtggcgataagtcgtgtcttaccgggttggactcaagacgatagttaccggataaggcgcagcggtcgggctgaacggggggttcgtgcacacagcccagcttggagcgaacgacctacaccgaactgagatacctacagcgtgagctatgagaaagcgccacgcttcccgaagggagaaaggcggacaggtatccggtaagcggcagggtcggaacaggagagcgcacgagggagcttccagggggaaacgcctggtatctttatagtcctgtcgggtttcgccacctctgacttgagcgtcgatttttgtgatgctcgtcaggggggcggagcctatggaaa".upper()
 PROMOTER_RNA_P = "TTGAGATCCTTTTTTTCTGCGCGTAATCTGCTGCTT"
 
 
 def get_alignment(fasta_file, seq):
     ali = get_promoter_alignment(fasta_file, seq)
-    print("done")
+    return ali
+
+
+def find_appropriate_combo(prom_alis: List[Bio.pairwise2.Alignment], seq_alis: List[Bio.pairwise2.Alignment]):
+    dist = 4000
+    best_combo = [0, 0]
+    for pa in prom_alis:
+        for sa in seq_alis:
+            curr_dist = abs(pa.start - sa.start)
+            if curr_dist < dist:
+                dist = curr_dist
+                best_combo[0] = pa
+                best_combo[1] = sa
+
+    return best_combo
+
+
+def get_ali_prom_and_seq(fasta_file, prom, seq):
+    gap_open_penalty = -3
+    gap_extend_penalty = -1
+
+    for record in SeqIO.parse(fasta_file, "fasta"):
+        sequence = str(record.seq)
+
+        sub_mat = substitution_matrices.load("NUC.4.4")
+        pairwise2.MAX_ALIGNMENTS = 10
+        prom_alignments = pairwise2.align.localds(prom, sequence, sub_mat,
+                                             gap_open_penalty, gap_extend_penalty)
+        seq_alignments = pairwise2.align.localds(seq, sequence, sub_mat,
+                                                  gap_open_penalty, gap_extend_penalty)
+
+        # Find the alignment with the highest score
+        # print(f"Found {len(alignments)} alignments for {fasta_file} and prom: {prom_seq}")
+        # best_alignment = max(alignments, key=lambda x: x.score)
+        best_combo = find_appropriate_combo(prom_alignments, seq_alignments)
+        return best_combo[0], best_combo[1]     # promotor_align, seq_align
 
 
 def match_promoter_vs_rna_p():
     folder = "fastas"
     for fasta in os.listdir(folder):
-        rna_p_ali = get_alignment(os.path.join(folder, fasta), RNA_P)
-        prom_ali = get_alignment(os.path.join(folder, fasta), PROMOTER_RNA_P)
-        print(f"matching promoter and rna p in {fasta}")
+        fasta_path = os.path.join(folder, fasta)
+        # prom_ali = get_alignment(fasta_path, PROMOTER_RNA_P)
+        # rna_p_ali = get_alignment(fasta_path, RNA_P)
+        prom_ali, seq_ali = get_ali_prom_and_seq(fasta_path, PROMOTER_RNA_P, RNA_P)
+        try:
+            print(f"{fasta}: alignment locations: promotor: {prom_ali.start}, rna_p seq: {seq_ali.start}, dist: {seq_ali.start - prom_ali.start}, gap prom end to seq start: {seq_ali.start - prom_ali.end}")
+        except Exception as ex:
+            print(str(ex))
 
 
 def parse_fastas():
@@ -149,15 +191,37 @@ def parse_fastas():
     df.to_csv("fastas_pairwise_RNAp.csv")
 
 
+def check_low_dist_fastas():
+    fastas_dir = "fastas"
 
+    with open(r"analysis\dist_check.txt", "r") as f:
+        data = [l.split(", ") for l in f.readlines()]
+
+    interesting_fastas = []
+    for l in data:
+        for sp in l:
+            if "dist" in sp:
+                curr_dist = int(sp.split(": ")[1])
+                if curr_dist < 100:
+                    interesting_fastas.append(l[0].split(": ")[0])
+
+    print(f"Found {len(interesting_fastas)}")
+    print("Now checking alignment")
+
+    align_dict = {}
+    for f in interesting_fastas:
+        alignment = get_promoter_alignment(os.path.join(fastas_dir, f), PROMOTER_RNA_P)
+        align_dict[f] = alignment
+
+    for f in align_dict:
+        print(f)
+        print(format_alignment(*align_dict[f]))
 
 
 # Todo:
 #  1) run MSA after pairwise
 #  2) try to consider specific length of promoter and make pairwise
 #  3) try to cluster promoters to groups
-
-
 
 
 if __name__ == '__main__':
@@ -171,6 +235,6 @@ if __name__ == '__main__':
     # for fasta_file in os.listdir(fastas_dir)[:10]:
     #     find_promoter_alignment_2(os.path.join(fastas_dir, fasta_file), promoter_sequence_p)
 
-    # main()
-
+    # match_promoter_vs_rna_p()
+    check_low_dist_fastas()
 
