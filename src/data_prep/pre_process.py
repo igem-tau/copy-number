7e9089e9186c176454dc473009e4ab6b8165a534
Tzlil <tzlilavidan@mail.tau.ac.il>
2023-08-07 17:29:52 +0300

    Added equal_bins_data function that returns bins labels and the binned df, called in saved_features_df function

--- src/data_prep/pre_process.py
+++ src/data_prep/pre_process.py
@@ -138,6 +138,12 @@ def split_for_testing(X: pd.DataFrame, y: Union[pd.DataFrame, pd.Series],
         reset_index(drop=True)
     return RNA_data_train_val, RNA_data_test
 
+def equal_bins_data(RNA_df,bins_num,zero_flag=0):
+    if zero_flag==1:
+        RNA_df['Copy Number'][RNA_df['Copy Number']<0]=0
+    RNA_df=RNA_df.sort_values(by=['Copy Number'])
+    bins_series=pd.qcut(RNA_df['Copy Number'],bins_num,labels=False).rename('stratify')
+    return RNA_df,bins_series
 
 def train_validation_split(X, y, stratify_by: pd.Series,
                            random_state: int = 0) -> Tuple[pd.DataFrame, pd.DataFrame, pd.Series, pd.Series]:
@@ -168,9 +174,14 @@ def save_features_df(p=True, i=True, shared=True, specify_date = False):
     if p:
         print('start generating RNAp features')
         RNAp_data = get_RNAp_data()
+        ## Tzlil
+        bins_num = 40
+        RNAp_data,RNAp_stratify_col = equal_bins_data(RNAp_data, bins_num)
         RNAp_X = RNAp_data.drop(TARGET_COLUMN, axis=1)
         RNAp_y = RNAp_data[TARGET_COLUMN]
-        RNAp_stratify_col = is_high_copy_number(RNAp_y).rename('stratify')
+
+        # RNAp_stratify_col = is_high_copy_number(RNAp_y).rename('stratify')
+
         RNAp_data_train_val, RNAp_data_test = split_for_testing(RNAp_X, RNAp_y, stratify_by=RNAp_stratify_col)
         RNAp_stratify_train_val, RNAp_stratify_test = split_for_testing(RNAp_stratify_col, RNAp_y,
                                                                             stratify_by=RNAp_stratify_col)
@@ -189,9 +200,12 @@ def save_features_df(p=True, i=True, shared=True, specify_date = False):
     if i:
         print('start generating RNAi features')
         RNAi_data = get_RNAi_data()
+        bins_num = 18
+        RNAi_data, RNAi_stratify_col = equal_bins_data(RNAi_data, bins_num)
         RNAi_X = RNAi_data.drop(TARGET_COLUMN, axis=1)
         RNAi_y = RNAi_data[TARGET_COLUMN]
-        RNAi_stratify_col = is_high_copy_number(RNAi_y).rename('stratify')
+
+        # RNAi_stratify_col = is_high_copy_number(RNAi_y).rename('stratify')
         RNAi_data_train_val, RNAi_data_test = split_for_testing(RNAi_X, RNAi_y, stratify_by=RNAi_stratify_col)
         RNAi_stratify_train_val, RNAi_stratify_test = split_for_testing(RNAi_stratify_col, RNAi_y,
                                                                             stratify_by=RNAi_stratify_col)
@@ -248,7 +262,7 @@ def get_features_df(p=True, i=True, shared=False, specify_date=False):
     if Path(DATA_PATH, 'DataFrames_with_features.joblib').exists():
         data = load(Path(DATA_PATH, 'DataFrames_with_features.joblib'))
     else:
-        from src.features.motifs import calc_motifs_pv
+        # from src.features.motifs import calc_motifs_pv
         data = save_features_df(p=p, i=i, shared=shared, specify_date=specify_date)
 
     return data
