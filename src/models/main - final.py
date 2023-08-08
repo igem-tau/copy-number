fbeff5756e4cdd262f5edcced479bcfebbe028c4
Tzlil <tzlilavidan@mail.tau.ac.il>
2023-08-08 20:13:55 +0300

    Combine sequences generator with the final main

--- src/models/main - final.py
+++ src/models/main - final.py
@@ -3,6 +3,7 @@ from src.analysis.forward_features_selection import multi_split_forward_selectio
 from src.utils import get_current_file_parent_path
 from src.data_prep.pre_process import get_RNAp_data, split_for_testing, create_fasta_file, get_features_df
 from xgboost import XGBRegressor
+from src.models.sequences_generator import sequence_generator
 
 CURRENT_FOLDER_PATH = get_current_file_parent_path(__file__)
 DATA_PATH = Path(CURRENT_FOLDER_PATH, '..', '..', 'data')
@@ -22,10 +23,15 @@ if __name__ == '__main__':
     # features selection
     fs_df = multi_split_forward_selection(data, rna_type='p', model=XGBRegressor(), model_type='xgboost')
     chosen_features = []
-    # hyperparameters tuning
+    # # hyperparameters tuning
 
     # train model
 
     # validation
 
     # save model and list of features
+
+    # Generate sequences and calculate features
+    generated_RNAp_seq = sequence_generator([(-33, -30), (-11, -8), (0, 0)], rna_type='p')
+    generated_RNAi_seq = sequence_generator([(-33, -30), (-11, -8), (0, 0)], rna_type = 'i')
+
