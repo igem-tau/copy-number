fca7f1f5dd17f4f1338e642dd870cfbb52bcbb44
rinatsaban <rinatsaban7@gmail.com>
2023-09-20 13:55:50 +0300

    all changes

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from pathlib import Path
 from src.analysis.EDA import EDA
 from src.consts import *
 from src.data_prep.pre_process import get_features_df, generate_features
-from src.models.Feature_Selection import feature_selection
+from src.models.Features_Models_Selection import feature_selection, model_selection
 from src.models.models_functions import model, scale
-from src.models.Parameters_Tuning.best_param_to_xl import get_best_params_set_xgb, get_best_param_xgb_optuna
+from src.models.Parameters_Tuning.best_param_to_xl import get_best_params_set_xgb, get_best_param_optuna
 from src.models.sequences_generator import sequence_df_generator
 from src.utils import get_current_file_parent_path, write_selected_features
 
@@ -35,54 +35,65 @@ if __name__ == '__main__':
     RNAp_y_test = data['RNAp_y_test']
     RNAp_stratify_test = data['RNAp_stratify_test']
 
-    # Feature selection
-    RNAp_selected_features_data = feature_selection(RNAp_X_train_features, RNAp_y_train)
-    RNAp_selected_features = RNAp_selected_features_data['selected_features']
+    # Feature and model selection
+    param_dict = model_selection(RNAp_X_train_features, RNAp_X_val_features, RNAp_y_train, RNAp_y_val)
+    models_fs_data = feature_selection(RNAp_X_train_features, RNAp_y_train, param_dict)
 
-    # write selected features to file
-    write_selected_features(RNAp_selected_features)
+    models = ['XGBoost'] # , 'CatBoostRegressor']
+    total_pred = []
+    final_predicted_dfs = []
+    for cur_model_name in models:
+        RNAp_selected_features_data = models_fs_data[cur_model_name]
+        RNAp_selected_features = RNAp_selected_features_data['selected_features']
 
-    # Data by selected features
-    RNAp_FS_train = RNAp_X_train_features[RNAp_selected_features]
-    RNAp_FS_val = RNAp_X_val_features[RNAp_selected_features]
-    RNAp_FS_test = RNAp_X_test_features[RNAp_selected_features]
+        # write selected features to file
+        write_selected_features(RNAp_selected_features)
 
-    # Exploratory Data Analysis (EDA)
-    EDA(RNAp_FS_train, RNAp_FS_val, RNAp_y_train, RNAp_y_val)
+        # Data by selected features
+        RNAp_FS_train = RNAp_X_train_features[RNAp_selected_features]
+        RNAp_FS_val = RNAp_X_val_features[RNAp_selected_features]
+        RNAp_FS_test = RNAp_X_test_features[RNAp_selected_features]
 
-    # Hyperparameters tuning
-    # Best_param_p_xgb = get_best_params_set_xgb(RNAp_FS_train, RNAp_FS_val, RNAp_y_train, RNAp_y_val, 'xgb_RNAp', RNAp_stratify_train_val)
-    Best_param_p_xgb = get_best_param_xgb_optuna(RNAp_FS_train, RNAp_FS_val, RNAp_y_train, RNAp_y_val)
+        # Exploratory Data Analysis (EDA)
+        # EDA(RNAp_FS_train, RNAp_FS_val, RNAp_y_train, RNAp_y_val)
 
-    # Run model
-    # TODO - Recalculate train_val with selected_features only while using the entire dataset
-    RNAp_FS_train_val_X = pd.concat([RNAp_FS_train, RNAp_FS_val])
-    RNAp_train_val_y = pd.concat([RNAp_y_train, RNAp_y_val])
-    trained_model, _, _, _ = model(RNAp_FS_train_val_X, RNAp_FS_test, RNAp_train_val_y, RNAp_y_test,
-                                   'xgboost', 'pRNA', Best_param_p_xgb, save_plots=True)
+        # Hyperparameters tuning
+        Best_param_p = get_best_param_optuna(RNAp_FS_train, RNAp_FS_val, RNAp_y_train, RNAp_y_val, cur_model_name)
 
-    # Generate sequences and calculate features
-    generated_RNAp_df = sequence_df_generator(rna_type='p')
+        # Run model
+        # TODO - Recalculate train_val with selected_features only while using the entire dataset
+        RNAp_FS_train_val_X = pd.concat([RNAp_FS_train, RNAp_FS_val])
+        RNAp_train_val_y = pd.concat([RNAp_y_train, RNAp_y_val])
+        trained_model, r2, mae_score, pearson, spearman, y_pred = model(RNAp_FS_train_val_X, RNAp_FS_test, RNAp_train_val_y, RNAp_y_test,
+                                       cur_model_name, 'pRNA', Best_param_p, save_plots=False)
+        total_pred.append(y_pred)
 
-    # Generate selected features
-    USE_SELECTED_FEATURES["selective"] = True
-    RNAp_train_val_data = pd.concat(
-        (pd.concat((RNAp_X_train_features, RNAp_X_val_features)), pd.concat((RNAp_y_train, RNAp_y_val))),
-        axis=1
-    )
-    # cp is False because RNA_y should be None because we need to predict the copy number
-    all_seqs_selected_features, _ = generate_features(generated_RNAp_df, reference_RNA_data=RNAp_train_val_data,
-                                                      cp=False)
+        # Generate sequences and calculate features
+        generated_RNAp_df = sequence_df_generator(rna_type='p')
 
-    # Predict
-    all_seqs_selected_features = all_seqs_selected_features[RNAp_selected_features]
-    _, all_seqs_selected_features_scaled = scale(RNAp_FS_train_val_X, all_seqs_selected_features)
-    y_pred = trained_model.predict(all_seqs_selected_features_scaled)
-    print(f"Range of copy nums predicted: {y_pred.min()} - {y_pred.max()}")
+        # Generate selected features
+        USE_SELECTED_FEATURES["selective"] = True
+        RNAp_train_val_data = pd.concat(
+            (pd.concat((RNAp_X_train_features, RNAp_X_val_features)), pd.concat((RNAp_y_train, RNAp_y_val))),
+            axis=1
+        )
+        # cp is False because RNA_y should be None because we need to predict the copy number
+        all_seqs_selected_features, _ = generate_features(generated_RNAp_df, reference_RNA_data=RNAp_train_val_data,
+                                                          cp=False,model_name=cur_model_name)
 
-    final_predicted_df = generated_RNAp_df[['Promoter Sequence (-35 to +1)']].join(pd.DataFrame({"copy number": y_pred}))
-    final_predicted_df.to_csv("copy_num_predictions.csv", index=False)
+        # Predict
+        all_seqs_selected_features = all_seqs_selected_features[RNAp_selected_features]
+        _, all_seqs_selected_features_scaled = scale(RNAp_FS_train_val_X, all_seqs_selected_features)
+        y_pred = trained_model.predict(all_seqs_selected_features_scaled)
+        print(f"Range of copy nums predicted: {y_pred.min()} - {y_pred.max()}")
 
-    save = True
-    if save:
-        trained_model.save_model(os.path.join(DATA_PATH, f'{str(pd.to_datetime("today")).split()[0]}_RNAp_model.json'))
+        final_predicted_df = generated_RNAp_df[['Promoter Sequence (-35 to +1)']].join(pd.DataFrame({"copy number": y_pred}))
+        final_predicted_dfs.append(final_predicted_df)
+        final_predicted_df.to_csv(f"copy_num_predictions_{cur_model_name}.csv", index=False)
+
+        # save models
+        trained_model.save_model(os.path.join(DATA_PATH, f'{str(pd.to_datetime("today")).split()[0]}_{cur_model_name}_RNAp_model.json'))
+
+    # TODO: combine the two models prediction
+
+
