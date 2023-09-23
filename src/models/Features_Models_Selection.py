a8231a1a0e46148e4d56640f3ec4bd8c2ab44ab1
iGem <igem@davids-MacBook-Pro.local>
2023-09-23 22:31:24 +0300

    parallelize mitual info correlation matrix calculations

--- src/models/Features_Models_Selection.py
+++ src/models/Features_Models_Selection.py
@@ -24,7 +24,7 @@ from typing import Dict
 
 from src.models.models_functions import scale
 from src.utils import get_current_file_parent_path, get_current_date
-from joblib import dump, load
+from joblib import dump, load, Parallel, delayed
 
 CURRENT_FOLDER_PATH = get_current_file_parent_path(__file__)
 DATA_PATH = Path(CURRENT_FOLDER_PATH, '..', '..', 'data')
@@ -310,26 +310,48 @@ def feature_selection(RNA_X, RNA_y, param_dict, models, rna_type):
         RNA_X_new = RNA_X.iloc[:, (mi > (mi.mean()))]
         new_mi = mi[(mi > (mi.mean()))]
 
+        # parallel approach for feature-feature correlation
+        # Define a function to calculate mutual information
+        def calculate_mutual_information(feature1, feature2, discrete_features_bool):
+            if feature2.dtype == 'int64':  # y is categorical
+                paired_mi = mutual_info_classif(feature1.to_numpy().reshape(-1, 1), feature2,
+                                         discrete_features=discrete_features_bool)
+            else:  # y is numerical
+                paired_mi = mutual_info_regression(feature1.to_numpy().reshape(-1, 1), feature2,
+                                            discrete_features=discrete_features_bool)
+            return paired_mi
+
+        def parallel_calculate_mi(i, j):
+            feature1 = RNA_X_new[new_features[i]]
+            feature2 = RNA_X_new[new_features[j]]
+            discrete_features_bool = feature1.dtype == 'int64'
+            paired_mi = calculate_mutual_information(feature1, feature2, discrete_features_bool)
+            return paired_mi
+
         # correlation between feature-feature (minimal) with MI
         print('Running: features selection - feature-feature')
         new_features = pd.Series(RNA_X_new.columns)
         corr_matrix = np.zeros((len(new_features), len(new_features)))
-        for i in range(len(new_features)):
-            for j in range(len(new_features)):
-                if i >= j:
-                    continue
-                discrete_features_bool = True if RNA_X_new[new_features[i]].dtype == 'int64' else False
-                if RNA_X_new[new_features[j]].dtype == 'int64':  # y is categorical
-                    corr_matrix[i, j] = mutual_info_classif(RNA_X_new[new_features[i]].to_numpy().reshape(-1, 1),
-                                                            RNA_X_new[new_features[j]],
-                                                            discrete_features=discrete_features_bool)
-                else:  # y is numerical
-                    corr_matrix[i, j] = mutual_info_regression(RNA_X_new[new_features[i]].to_numpy().reshape(-1, 1),
-                                                               RNA_X_new[new_features[j]],
-                                                               discrete_features=discrete_features_bool)
 
+        # Create pairs of feature indices for upper triangle calculation
+        pairs = [(i, j) for i in range(len(new_features)) for j in range (i+1, len(new_features))]
+
+        # Parallelize the mutual information calculation for upper triangle
+        mi_values_upper = Parallel(n_jobs=-2)(delayed(parallel_calculate_mi)(i, j) for i, j in tqdm(pairs))
+
+        # Fill the correlation matrix for the upper triangle
+        corr_matrix[np.array(pairs)[:, 0], np.array(pairs)[:, 1]] = mi_values_upper
+        # for i in range(len(new_features)):
+        #     for j in range(len(new_features)):
+        #         if i >= j:
+        #             continue
+        #         corr_matrix[i, j] = parallel_calculate_mi(i, j)
+
+        #  Transform the lower triangle to np.nan
         il1 = np.tril_indices(len(new_features))
         corr_matrix[il1] = np.nan
+
+        dump(corr_matrix, Path(DATA_PATH, 'mitual_info_matrix.joblib'), compress=True)
 
         (row, col) = (corr_matrix > np.nanquantile(corr_matrix, 0.99)).nonzero()  # TODO: think of different condition
 
