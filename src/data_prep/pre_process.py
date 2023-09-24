6825d36c6c241aa6ac2e6fdc583c9c81e14dd68b
iGem <igem@davids-MacBook-Pro.local>
2023-09-24 18:06:21 +0300

    update generate_features, upgrade kmers nucleotide features to new style

--- src/data_prep/pre_process.py
+++ src/data_prep/pre_process.py
@@ -130,7 +130,8 @@ def generate_features(RNA_data: pd.DataFrame, rna_type: str = 'p',
 
     RNA_features.append(calc_motifs_pv(RNA_seq, selected_features))
     RNA_features.append(generate_one_hot_encoding(RNA_seq, selected_features))
-    RNA_features.append(extract_nucli_features(RNA_seq))
+    RNA_features.append(extract_nucli_features(RNA_seq, selected_features))
+    # TODO - continue update selected features style from here
     RNA_features.append(
         calc_promoter_zones_strength(RNA_seq, RNAp_EDITED_ZONES if rna_type == 'p' else RNAi_EDITED_ZONES))
     RNA_features.append(entropy(RNA_seq))
