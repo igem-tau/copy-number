ba0cb4f45660d1d27f0812a35c0551974e7c5d49
iGem <igem@davids-MacBook-Pro.local>
2023-09-24 14:52:39 +0300

    reformat EDA.py file

--- src/analysis/EDA.py
+++ src/analysis/EDA.py
@@ -5,54 +5,57 @@ from src.utils import get_current_file_parent_path, get_current_date
 from pathlib import Path
 import plotly.express as px
 
-
 CURRENT_FOLDER_PATH = get_current_file_parent_path(__file__)
 DATA_PATH = Path(CURRENT_FOLDER_PATH, '..', '..', 'data')
+
 
 def plot_features_dist(df):
     features = df.columns
     fig = make_subplots(rows=2, cols=5, subplot_titles=features)
-    for i in range(len(features)):
-        if features[i]=='Fis_26-25':
-            cur_data = df[features[i]][df[features[i]]!=1]
+    for i, feature in enumerate(features):
+        if feature == 'Fis_26-25':
+            cur_data = df[feature][df[feature] != 1]
         else:
-            cur_data = df[features[i]]
-        if i<5:
-            fig.add_trace(go.Histogram(x=cur_data, name=features[i]), row=1, col=i+1)
+            cur_data = df[feature]
+        if feature < 5:
+            fig.add_trace(go.Histogram(x=cur_data, name=feature), row=1, col=i + 1)
         else:
-            fig.add_trace(go.Histogram(x=cur_data, name=features[i]), row=2, col=i-4)
+            fig.add_trace(go.Histogram(x=cur_data, name=feature), row=2, col=i - 4)
     fig.update_yaxes(title_text="Counts", row=1, col=1)
     fig.update_yaxes(title_text="Counts", row=2, col=1)
     fig.update_layout(template='plotly_white', title_text='<b>Features Distributions', title_x=0.5)
     return fig
 
+
 def plot_features_box(df):
     numeric_features = df[['Fis_26-25', 'at_skew', 'dG_total']]
     fig = go.Figure()
-    for i in range(len(numeric_features.columns)):
-        if numeric_features.columns[i]=='Fis_26-25':
-            cur_data = numeric_features.iloc[:,i][numeric_features.iloc[:,i]!=1]
+    for col in numeric_features.columns:
+        if col == 'Fis_26-25':
+            cur_data = numeric_features[col][numeric_features[col] != 1]
         else:
-            cur_data = numeric_features.iloc[:, i]
-        fig.add_trace(go.Box(y=cur_data, boxpoints='all', name=numeric_features.columns[i]))
+            cur_data = numeric_features[col]
+        fig.add_trace(go.Box(y=cur_data, boxpoints='all', name=col))
 
-    fig.update_layout(template='plotly_white', title='<b>Features Boxplots', title_x=0.5,yaxis_title_text='Features values')
+    fig.update_layout(template='plotly_white', title='<b>Features Boxplots', title_x=0.5,
+                      yaxis_title_text='Features values')
     return fig
+
 
 def plot_scatter_hist(df_x, df_y):
     fig = make_subplots(rows=2, cols=5, subplot_titles=df_x.columns)
     numeric_features = df_x[['Fis_26-25', 'at_skew', 'dG_total']]
     discrete_features = df_x[[col for col in df_x.columns if col not in numeric_features.columns]]
 
-    for i in range(len(df_x.columns)):
-        if df_x.columns[i] in discrete_features:
-            plot = px.violin(x=df_x.iloc[:,i], y=df_y)
+    for i, col in enumerate(df_x.columns):
+        if col in discrete_features:
+            plot = px.violin(x=df_x[col], y=df_y)
         else:
-            if df_x.columns[i] == 'Fis_26-25':
-                cur_data = df_x.iloc[:,i][df_x.iloc[:,i] != 1]
-                cur_df_y = df_y[df_x.iloc[:,i] != 1]
+            if col == 'Fis_26-25':
+                cur_data = df_x[col][df_x[col] != 1]
+                cur_df_y = df_y[df_x[col] != 1]
             else:
-                cur_data = df_x.iloc[:,i]
+                cur_data = df_x[col]
                 cur_df_y = df_y
             plot = px.scatter(
                 x=cur_data,
@@ -70,7 +73,8 @@ def plot_scatter_hist(df_x, df_y):
     return fig
 
 
-def exploratory_data_analysis(train: pd.DataFrame, val: pd.DataFrame, y_train: pd.Series, y_val: pd.Series, rna_type: str) -> None:
+def exploratory_data_analysis(train: pd.DataFrame, val: pd.DataFrame, y_train: pd.Series, y_val: pd.Series,
+                              rna_type: str) -> None:
     df_x = pd.concat([train, val])
     df_y = pd.concat([y_train, y_val])
     fig1 = plot_features_dist(df_x)
@@ -82,5 +86,3 @@ def exploratory_data_analysis(train: pd.DataFrame, val: pd.DataFrame, y_train: p
         f.write(fig3.to_html(full_html=False, include_plotlyjs='cdn'))
         f.write(fig1.to_html(full_html=False, include_plotlyjs='cdn'))
         f.write(fig2.to_html(full_html=False, include_plotlyjs='cdn'))
-
-
