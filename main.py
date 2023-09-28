75b5ede462870a0a62d8ea14d644b60c889f353f
rinatsaban <rinatsaban7@gmail.com>
2023-09-28 15:10:28 +0300

    added voting model results

--- main.py
+++ main.py
@@ -5,10 +5,11 @@ from src.analysis.EDA import exploratory_data_analysis
 from src.consts import *
 from src.data_prep.pre_process import get_features_df, generate_features
 from src.models.Features_Models_Selection import feature_selection, model_selection
-from src.models.models_functions import model, scale
+from src.models.models_functions import model, scale, estimate_pred
 from src.models.Parameters_Tuning.best_param_to_xl import get_best_params_set_xgb, get_best_param_optuna
 from src.models.sequences_generator import sequence_df_generator
 from src.utils import get_current_file_parent_path, get_current_date
+import numpy as np
 
 CURRENT_FOLDER_PATH = get_current_file_parent_path(__file__)
 DATA_PATH = Path(CURRENT_FOLDER_PATH, 'data')
@@ -28,16 +29,16 @@ def run_pipeline(rna_type: str):
     RNA_X_test_features = data[f'RNA{rna_type}_X_test']
     RNA_y_test = data[f'RNA{rna_type}_y_test']
 
+
     # Feature and model selection
     param_dict = model_selection(RNA_X_train_features, RNA_X_val_features, RNA_y_train, RNA_y_val, rna_type)
-    models = ['XGBoost']  # , 'CatBoostRegressor']
-    models = ['CatBoostRegressor']
-    models_fs_data = feature_selection(RNA_X_train_features, RNA_y_train, param_dict, models, rna_type)
+    models = ['XGBoost', 'CatBoostRegressor']
+    # models = ['CatBoostRegressor']
 
     total_pred = []
     final_predicted_dfs = []
     for cur_model_name in models:
-        RNA_selected_features_data = models_fs_data[cur_model_name]
+        RNA_selected_features_data = feature_selection(RNA_X_train_features, RNA_y_train, param_dict, cur_model_name, rna_type)
         RNA_selected_features = RNA_selected_features_data['selected_features']
 
         # Data by selected features
@@ -49,8 +50,9 @@ def run_pipeline(rna_type: str):
         # if rna_type == 'p':
         #     exploratory_data_analysis(RNA_FS_train, RNA_FS_val, RNA_y_train, RNA_y_val, rna_type)
 
+
         # Hyperparameters tuning
-        # Best_param_xgb = get_best_params_set_xgb(RNA_FS_train, RNA_FS_val, RNA_y_train, RNA_y_val,
+        # Best_params = get_best_params_set_xgb(RNA_FS_train, RNA_FS_val, RNA_y_train, RNA_y_val,
         #                                          f'xgb_RNA{rna_type}')
         Best_params = get_best_param_optuna(RNA_FS_train, RNA_FS_val, RNA_y_train, RNA_y_val, cur_model_name, rna_type)
 
@@ -96,8 +98,12 @@ def run_pipeline(rna_type: str):
         )
 
     # TODO: combine the two models prediction
+    final_pred = np.array(total_pred).mean(axis=0)
+    estimate_pred(RNA_y_test, final_pred, 'voting model')
+
+    # final_predicted_df
 
 
 if __name__ == '__main__':
-    # run_pipeline(rna_type='p')
-    run_pipeline(rna_type='i')
+    run_pipeline(rna_type='p')
+    # run_pipeline(rna_type='i')
