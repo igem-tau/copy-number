dd41ceadece1c8c46ffffab0f9e5d87bbc5cf764
Dell <yamtawachi@mail.tau.ac.il>
2023-10-06 12:32:44 +0300

    confident script updated-now supports both rnai,rnap (xgb and catboost) + generates a score+graph. When all files are ready the paths in the main section need to be updated.

--- src/generate_confident.py
+++ src/generate_confident.py
@@ -1,3 +1,4 @@
+import catboost
 import numpy as np
 import pandas as pd
 import xgboost
@@ -5,6 +6,8 @@ from joblib import load
 import matplotlib.pyplot as plt
 from pathlib import Path
 from src.utils import get_current_file_parent_path
+from catboost import CatBoostRegressor
+
 from src.data_prep.pre_process import get_features_df
 
 CURRENT_FOLDER_PATH = get_current_file_parent_path(__file__)
@@ -33,18 +36,27 @@ def generate_conf_plot(hparam_path, x_train, y_train, X_test, y_test):
     common_params['n_estimators'] = int(common_params['n_estimators'])
     alpha = 0.05
     prediction_interval_percent = int((1 - 2 * alpha) * 100)
-    for limit in [alpha, 1 - alpha]:
-        gbr = xgboost.XGBRegressor(objective=log_cosh_quantile(limit), **common_params)
-        all_models["q %1.2f" % limit] = gbr.fit(x_train, y_train)
-    gbr_ls = xgboost.XGBRegressor(**common_params)
+    if rna_type == 'p':
+        for limit in [alpha, 1 - alpha]:
+            gbr = xgboost.XGBRegressor(objective=log_cosh_quantile(limit), **common_params)
+            all_models["q %1.2f" % limit] = gbr.fit(x_train, y_train)
+        gbr_ls = xgboost.XGBRegressor(**common_params)
+    elif rna_type=='i':
+        for limit in [alpha, 1 - alpha]:
+            gbr =catboost.CatBoostRegressor(loss_function=f'Quantile:alpha={limit}', **common_params)
+            all_models["q %1.2f" % limit] = gbr.fit(x_train, y_train)
+        gbr_ls = catboost.CatBoostRegressor(**common_params)
+
+    else:
+        raise 'RNA TYPE NOT SUPPORTED'
 
     all_models["mse"] = gbr_ls.fit(x_train, y_train)
 
     y_pred = all_models["mse"].predict(X_test_sorted)
     y_lower = all_models["q 0.05"].predict(X_test_sorted)
     y_upper = all_models["q 0.95"].predict(X_test_sorted)
-
     def draw_plot(q_lower, q_upper, real_values, save_plot=True):
+        accu = 100 * len(real_values[(q_upper - real_values > 0) * (y_pred - q_lower > 0)]) / len(real_values)
         fig = plt.figure(figsize=(10, 10))
         plt.plot(real_values, 'gx', label=u'real value')
         plt.plot(q_lower, 'b_', label=u'Q low')
@@ -53,33 +65,25 @@ def generate_conf_plot(hparam_path, x_train, y_train, X_test, y_test):
         plt.fill(np.concatenate([index, index[::-1]]),
                  np.concatenate([q_lower, q_upper[::-1]]),
                  alpha=.25, fc='b', ec='None', label=f'{prediction_interval_percent}% prediction interval')
-        # plt.plot(real_values, q_upper, "k-")
-        # plt.plot(real_values, q_lower, "k-")
-        # plt.fill_between(
-        #     real_values, q_lower, q_upper, alpha=0.25, label=f'Predicted {prediction_interval_percent}% interval'
-        # )
         plt.grid()
         plt.plot(real_values, y_pred, 'o', label='Predicted mean')
-
-        # plt.xlabel("y_pred")
         plt.xlabel('$index$')
-        # plt.ylabel("y_test")
         plt.ylabel('$PCN$')
         plt.legend(loc="upper left")
-        plt.title(f'confident score for RNA{rna_type}')
+        plt.title(f'confident score for RNA{rna_type}-{accu}%')
         plt.show()
         if save_plot:
-            fig.savefig(f'{DATA_PATH}\\conf_score_RNA{rna_type}_XGBoost.png')
+            fig.savefig(f'{DATA_PATH}\\conf_score_RNA{rna_type}.png')
 
     draw_plot(y_lower, y_upper, y_test_sorted.ravel())
 
 
-if __name__ == '__main__':
+if __name__ == '__main__': #TODO- use correct files for both rnai rnap
     rna_type = 'p'
     hparam_path = Path(DATA_PATH, f'RNA{rna_type}_best_params_XGBoost.joblib')
     selected_features = load(Path(DATA_PATH, f'RNA{rna_type}_XGBoost_Selected_Features.joblib'))['selected_features']
 
-    data = get_features_df(rna_type=rna_type)
+    data =load(r'C:\Users\Dell\PycharmProjects\pythonProject\copy-number\data\RNAp_DataFrame_with_features.joblib')#get_features_df(rna_type=rna_type)
 
     RNA_X_train_features = data[f'RNA{rna_type}_X_train'][selected_features]
     RNA_y_train = data[f'RNA{rna_type}_y_train']
