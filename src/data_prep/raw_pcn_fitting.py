0540c429de2096c33006b86a27ef8ec43d5eacd9
iGem <igem@davids-MacBook-Pro.local>
2023-10-09 22:56:28 +0300

    update the pcn with the biology measurements of the sequences used for fitting

--- src/data_prep/raw_pcn_fitting.py
+++ src/data_prep/raw_pcn_fitting.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-from src.consts import PROMOTER_LENGTH
+from src.consts import PROMOTER_LENGTH, RNA_DATA_COLUMNS, TARGET_COLUMN
 from src.utils import get_current_file_parent_path
 from pathlib import Path
 from scipy.stats import linregress
@@ -24,7 +24,11 @@ def join_data_with_article_sequences(article_data, additional_data, additional_p
     return merged_data
 
 
-def get_measured_pcn():
+def filter_measurements_fot_fit(measurements_df):
+    return measurements_df.query('use_for_fit == "V"')
+
+
+def get_measured_pcn(with_duplicates=True, for_fit=False):
     article_data = pd.read_csv(DATA_FILE_PATH, index_col=0)
     article_ddpcr_data = pd.read_excel(ADDITIONAL_DATA_FILE_PATH, sheet_name=f'ddPCR - RNA{RNA_TYPE}')[
         ['promoter seq', 'use for fit', 'ddpcr']]
@@ -39,18 +43,24 @@ def get_measured_pcn():
 
     raw_target_pcn_df = pd.concat((
         pd.DataFrame(
-            {'raw_pcn': concat_ddpcr[RAW_PCN_COLUMN_NAME], 'target_pcn': concat_ddpcr[TARGET_PCN_COLUMN_NAME],
+            {RNA_DATA_COLUMNS[0]: concat_ddpcr[RNA_DATA_COLUMNS[0]], 'raw_pcn': concat_ddpcr[RAW_PCN_COLUMN_NAME],
+             'target_pcn': concat_ddpcr[TARGET_PCN_COLUMN_NAME],
              'use_for_fit': concat_ddpcr['use for fit'], 'type': 'ddPCR'}),
         pd.DataFrame(
-            {'raw_pcn': concat_qpcr[RAW_PCN_COLUMN_NAME], 'target_pcn': concat_qpcr[TARGET_PCN_COLUMN_NAME],
+            {RNA_DATA_COLUMNS[0]: concat_qpcr[RNA_DATA_COLUMNS[0]], 'raw_pcn': concat_qpcr[RAW_PCN_COLUMN_NAME],
+             'target_pcn': concat_qpcr[TARGET_PCN_COLUMN_NAME],
              'use_for_fit': concat_qpcr['use for fit'], 'type': 'qPCR'})),
         axis=0)
 
-    return raw_target_pcn_df
+    if for_fit:
+        raw_target_pcn_df = filter_measurements_fot_fit(raw_target_pcn_df)
 
-
-def filter_measurements_fot_fit(measurements_df):
-    return measurements_df.query('use_for_fit == "V"')
+    if with_duplicates:
+        return raw_target_pcn_df
+    else:
+        # prefer qPCR over ddPCR
+        return raw_target_pcn_df.sort_values(by='type', ascending=False).drop_duplicates(keep='first',
+                                                                                         subset=RNA_DATA_COLUMNS[0])
 
 
 def get_log_log_linear_regression(raw_pcn, target_pcn):
@@ -67,7 +77,16 @@ def apply_linear_fit(slope, intercept):
 
 
 def custom_fit_and_transform_raw_pcn(raw_pcn):
-    measured_pcn = get_measured_pcn()
+    measured_pcn = get_measured_pcn(for_fit=True)
     measured_pcn_for_fit = filter_measurements_fot_fit(measured_pcn)
-    slope, intercept = get_log_log_linear_regression(measured_pcn_for_fit['raw_pcn'], measured_pcn_for_fit['target_pcn'])
+    slope, intercept = get_log_log_linear_regression(measured_pcn_for_fit['raw_pcn'],
+                                                     measured_pcn_for_fit['target_pcn'])
     return raw_pcn.apply(apply_linear_fit(slope, intercept))
+
+
+def update_pcn_by_biology_results(RNAp_df):
+    measured_pcn = get_measured_pcn(with_duplicates=False, for_fit=True)
+    merged_measured_pcn = RNAp_df.merge(measured_pcn, on=RNA_DATA_COLUMNS[0], how='left')
+    return merged_measured_pcn.apply(
+        lambda measurement: measurement['target_pcn'] if not np.isnan(measurement['target_pcn']) else measurement[
+            TARGET_COLUMN], axis=1)
